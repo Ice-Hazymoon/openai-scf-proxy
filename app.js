25875a3d0a64b5d0aaf6b9220cad8efb3079c819
libp <924701358@qq.com>
2023-11-04 20:31:40 +0800

    update

--- app.js
+++ app.js
@@ -6,50 +6,46 @@ const {
 const app = express()
 const port = 9000
 
-// app.use('/', createProxyMiddleware({
-//   target: 'https://api.openai.com/',
-//   changeOrigin: true,
-//   onProxyReq: (proxyReq, req, res) => {
-//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-//     proxyReq.removeHeader('x-forwarded-for');
-//     proxyReq.removeHeader('x-real-ip');
-//   },
-//   onProxyRes: function (proxyRes, req, res) {
-//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-//   }
-// }));
-
-app.use('/', (req, res) => {
-  const targetUrl = req.query.url; // 从查询参数中获取目标 URL
-  req.pipe(request(targetUrl)).pipe(res);
-});
+app.use('/openai', createProxyMiddleware({
+  target: 'https://api.openai.com/',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
 
 
-// app.use('/google', createProxyMiddleware({
-//   target: 'https://google.com/',
-//   changeOrigin: true,
-//   onProxyReq: (proxyReq, req, res) => {
-//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-//     proxyReq.removeHeader('x-forwarded-for');
-//     proxyReq.removeHeader('x-real-ip');
-//   },
-//   onProxyRes: function (proxyRes, req, res) {
-//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-//   }
-// }));
 
-// app.use('/github', createProxyMiddleware({
-//   target: 'https://github.com/',
-//   changeOrigin: true,
-//   onProxyReq: (proxyReq, req, res) => {
-//     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-//     proxyReq.removeHeader('x-forwarded-for');
-//     proxyReq.removeHeader('x-real-ip');
-//   },
-//   onProxyRes: function (proxyRes, req, res) {
-//     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-//   }
-// }));
+app.use('/google', createProxyMiddleware({
+  target: 'https://google.com/',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.use('/github', createProxyMiddleware({
+  target: 'https://github.com/',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
