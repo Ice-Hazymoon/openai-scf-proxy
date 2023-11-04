982de8ae47aaf46a6fbd5cf69fe405cbdf553450
libp <924701358@qq.com>
2023-11-04 19:20:58 +0800

    update

--- app.js
+++ app.js
@@ -6,7 +6,33 @@ const app = express()
 const port = 9000
 
 app.use('/', createProxyMiddleware({
-  target: 'https://api.openai.com',
+  target: 'https://github.com/',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.use('/google', createProxyMiddleware({
+  target: 'https://google.com/',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.use('/openai', createProxyMiddleware({
+  target: 'https://api.openai.com/',
   changeOrigin: true,
   onProxyReq: (proxyReq, req, res) => {
     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
